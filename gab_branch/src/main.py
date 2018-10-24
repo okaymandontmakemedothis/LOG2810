643f13437a824a2d80483a35366cc0d57414b072
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2018-10-24 17:38:55 -0400

    Update gab_branch/src/main.py
    more comments

--- gab_branch/src/main.py
+++ gab_branch/src/main.py
@@ -9,15 +9,16 @@ with open('centresLocaux.txt') as f:
 	firstSection = True
 	for x in list(f):
 		if x == '\n':
-			firstSection = False
+			firstSection = False #firstSection: first half of the .txt with info to create Nodes
 		else:
 			info = x.split(',')
 			info = [int(i) for i in info]
 
-			if firstSection:
+			if firstSection: #create all the nodes with the available info from the .txt and add them to the graph (no edges yet)
 				node = Node(info[0], info[1])
 				g.addNode(node)
-			else:
+			else: #create the edges
+				#Edge(node1, node2, cost)
 				edge = Edge(g.getNodes()[info[0]], g.getNodes()[info[1]], info[2])
 
 
