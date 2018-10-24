ad2d8f5a036d6c2dbb726a5a07412dc0e3b8c9fd
gabrielhv <11673590+gabrielhv@users.noreply.github.com>
2018-10-24 16:51:33 -0400

    comments added

--- gab_branch/src/graphe.py
+++ gab_branch/src/graphe.py
@@ -1,5 +1,9 @@
 
-
+# Represent a node in the graph
+# param:
+# 	id: number associated with the node
+#	recharge: node has a recharge point (1) or not (0)
+#	edges: list of the edges connected to the current node
 class Node:
 	def __init__(self, id, recharge):
 		self.id = id
@@ -18,13 +22,20 @@ class Node:
 	def addEdge(self, e):
 		self.edges.append(e)
 
+
+	# Simple print of every edge's cost linked to the current node
 	def printEdges(self):
 		for x in self.edges:
 			print(x.getCost())
 		
 
-
+# Represent an edge linking 2 nodes
+# param:
+#	node1, node2: nodes linked togheter
+#	cost: cost associated to the edge
 class Edge:
+	
+	# When built, an edge adds itself to the 2 nodes it links togheter
 	def __init__(self, node1, node2, cost):
 		self.node1 = node1
 		self.node2 = node2
@@ -42,6 +53,9 @@ class Edge:
 		return self.node2
 
 
+# Represent the full graph
+# param:
+#	nodes: list of nodes currently in the graph
 class Graphe(object):
 	def __init__(self):
 		self.nodes = {}
@@ -52,6 +66,7 @@ class Graphe(object):
 	def addNode(self, node):
 		self.nodes[node.getId()] = node
 
+	# Simple print of every nodes id's and their edges cost
 	def printGraph(self):
 		for x in self.nodes:
 			print("Noeud: ", x)
